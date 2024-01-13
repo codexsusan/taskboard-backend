aaaaa34383cb54928a5e71230768dfd0009678ec
codexsusan <susankhadka708@gmail.com>
2024-01-13 22:30:05 +0530

    Minor changes in script and port number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const port = process.env.PORT ;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
